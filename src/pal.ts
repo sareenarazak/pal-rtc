a826321e871f29191eb96fb5bd1e1b9ee04fcbe6
sareenaabdulrazak <11263177+sareenarazak@users.noreply.github.com>
2024-02-29 02:17:09 -0500

    Add textarea input and send event handling

--- src/pal.ts
+++ src/pal.ts
@@ -10,11 +10,25 @@ const clientConfig: ClientConfig = {
     peerClientIds: new Set<string>()
 };
 
+type PeerConnection = {
+    peerConnection: RTCPeerConnection | undefined;
+    destinationId: string | undefined;
+    dataChannel: RTCDataChannel | undefined;
+}
+
+const peerConn: PeerConnection = {
+    peerConnection: undefined,
+    destinationId: undefined,
+    dataChannel: undefined
+}
+
 let ws: WebSocket;
 
 document.addEventListener("DOMContentLoaded", () => {
     const connectBtn = document.querySelector("#ws-connect") as HTMLButtonElement;
     const disconnectBtn = document.querySelector("#ws-disconnect") as HTMLButtonElement;
+    const textArea = document.querySelector("#ws-text") as HTMLTextAreaElement;
+    const sendBtn = document.querySelector("#send-message") as HTMLButtonElement;
 
     connectBtn.addEventListener("click", () => {
         connect.call(connectBtn, disconnectBtn);
@@ -23,6 +37,18 @@ document.addEventListener("DOMContentLoaded", () => {
     disconnectBtn.addEventListener("click", () => {
         disconnect.call(disconnectBtn, connectBtn)
     });
+
+    // TODO : change this to some event that only triggers once
+    textArea.addEventListener("input",  () => {
+        if (sendBtn.disabled) {
+            sendBtn.disabled = false
+        }
+    });
+
+    sendBtn.addEventListener("click",  () => {
+        sendToPal.call(sendBtn, textArea.value);
+    });
+
 });
 
 function connect(this: HTMLButtonElement, disconnBtn: HTMLButtonElement) {
@@ -37,7 +63,7 @@ function connect(this: HTMLButtonElement, disconnBtn: HTMLButtonElement) {
             try {
                 console.log(`Received message from server ${event.data}`);
                 const message: Message = JSON.parse(event.data);
-                handleMessage(message)
+                handleMessage(message);
             } catch (error) {
                 if (error instanceof Error) {
                     console.error(`Error while handling message from server Error : ${error.message}`);
@@ -79,5 +105,17 @@ function handleMessage(message : Message) {
         default:
             throw new Error(`Unsupported message type ${type}`);
     }
+}
 
+function sendToPal(this:HTMLButtonElement, message: string) {
+    const peerConnection = peerConn.peerConnection;
+    if (peerConnection && peerConnection.connectionState === "connected") {
+        if (clientConfig.peerClientIds.size !== 0) {
+            // TODO : Add data channel creation
+        } else {
+            console.log("No peers yet");
+        }
+    } else {
+        console.log(message);
+    }
 }
